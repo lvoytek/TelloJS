ca32b8a4ef56fd3331757150f4d2016fe7afed8f
Lucas Boje <lucas.boje@stsaviationgroup.com>
2020-06-14 13:02:04 -0700

    Added takeoff & landing buttons

--- scripts/Tello.js
+++ scripts/Tello.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (c) 2020 Lena Voytek, Lucas Boje
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -7,10 +7,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,9 +18,17 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
- * 
+ *
+ *
  * Tello.js
  * This is the main JavaScript file that uses functions from the other js files
- * to associate GUI items with drone commands. 
+ * to associate GUI items with drone commands.
  */
+
+function takeOff() {
+  alert("Take off!");
+}
+
+function landDrone() {
+  alert("Landing");
+}
