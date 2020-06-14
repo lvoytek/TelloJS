25fd6a7346ceea76be2c5c2746cbd06210715c4c
Lena Voytek <dvoytek@email.arizona.edu>
2020-06-14 13:44:47 -0700

    setup of app.js

--- app.js
+++ app.js
@@ -5,13 +5,28 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var droneCmdRouter = require('./routes/dronecmd');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(function (req, res, next)
+{
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	// Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	// Pass to next layer of middleware
+	next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +35,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/dronecmd', droneCmdRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
