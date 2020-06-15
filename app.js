38fdaf80aee3b598a21547bdc1acbf3d3a741a62
Lucas Boje <lucas.boje@stsaviationgroup.com>
2020-06-14 20:11:11 -0700

    Video streaming working on windows :)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (c) 2020 Lena Voytek, Lucas Boje
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -7,10 +7,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,105 +20,115 @@
  * SOFTWARE.
  */
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var webSocket = require('ws');
-var http = require('http');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var webSocket = require("ws");
+var http = require("http");
 
-const spawn = require('child_process').spawn;
+const spawn = require("child_process").spawn;
 
-var droneCmdRouter = require('./routes/dronecmd');
+var droneCmdRouter = require("./routes/dronecmd");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(function (req, res, next)
-{
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	// Request methods you wish to allow
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	// Pass to next layer of middleware
-	next();
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
 });
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/dronecmd', droneCmdRouter);
+app.use("/dronecmd", droneCmdRouter);
 
+const videoStreamServer = http
+  .createServer(function (req, res) {
+    req.on("data", function (data) {
+      webSocketServer.broadcast(data);
+    });
+  })
+  .listen(3001);
 
-const videoStreamServer = http.createServer(function(req, res)
-{
-	req.on('data', function(data)
-	{
-		webSocketServer.broadcast(data);
-	});
-}).listen(3001);
-
-const webSocketServer = new webSocket.Server(
-{
-	server : videoStreamServer
+const webSocketServer = new webSocket.Server({
+  server: videoStreamServer,
 });
 
-webSocketServer.broadcast = function(data)
-{
-	webSocketServer.clients.forEach(function each(client)
-	{
-		if(client.readyState === webSocket.OPEN)
-			client.send(data);
-	});
+webSocketServer.broadcast = function (data) {
+  webSocketServer.clients.forEach(function each(client) {
+    if (client.readyState === webSocket.OPEN) client.send(data);
+  });
 };
 
-setTimeout(function()
-{
-	var args = [
-		"-i", "udp://0.0.0.0:11111",
-		"-r", "30",
-		"-s", "960x720",
-		"-codec:v", "mpeg1video",
-		"-b", "800k",
-		"-f", "mpegts",
-		"http://127.0.0.1:3001/stream"
-	];
+setTimeout(function () {
+  let isWin = process.platform === "win32";
 
-	// Spawn an ffmpeg instance
-	var streamer = spawn('ffmpeg', args);
-	
-	streamer.on("exit", function(code)
-	{
-		console.log("Failure", code);
-	});
+  var args = [
+    "-i",
+    "udp://0.0.0.0:11111",
+    "-r",
+    "30",
+    "-s",
+    "960x720",
+    "-codec:v",
+    "mpeg1video",
+    "-b",
+    "800k",
+    "-f",
+    "mpegts",
+    "http://127.0.0.1:3001/stream",
+  ];
+
+  // Spawn an ffmpeg instance
+  var streamer = isWin
+    ? spawn("ffmpeg.exe", args, { shell: true })
+    : spawn("ffmpeg", args);
+
+  streamer.stderr.pipe(process.stderr);
+
+  streamer.on("exit", function (code) {
+    console.log("Failure", code);
+  });
 }, 3000);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- public/scripts/Tello.js
+++ public/scripts/Tello.js
@@ -29,77 +29,72 @@ const refreshRate = 100;
 var droneControlMode = false;
 var videoMode = false;
 
-$(document).ready(function()
-{
-	const output = $("#output");
+$(document).ready(function () {
+  const output = $("#output");
 
-	setInterval(controllerStart, refreshRate);
+  setInterval(controllerStart, refreshRate);
 
-	function controllerStart() {
-		const gamepads = navigator.getGamepads();
-		const gamepad = gamepads[0];
+  function controllerStart() {
+    const gamepads = navigator.getGamepads();
+    const gamepad = gamepads[0];
 
-		// Escape if no gamepad was found
-		if (!gamepad) {
-			console.log("No gamepad found.");
-			return;
-		}
+    // Escape if no gamepad was found
+    if (!gamepad) {
+      console.log("No gamepad found.");
+      return;
+    }
 
-		// Filter out only the buttons which are pressed
-		const pressedButtons = gamepad.buttons
-			.map((button, id) => ({ id, button }))
-			.filter(isPressed);
+    // Filter out only the buttons which are pressed
+    const pressedButtons = gamepad.buttons
+      .map((button, id) => ({ id, button }))
+      .filter(isPressed);
 
-		for (const button of pressedButtons) {
-			console.log(button);
-			if (droneControlMode === false && button.id == 9) {
-				connectDrone();
-				droneControlMode = true;
-			} else {
-				droneController(button.id);
-			}
-		}
-	}
+    for (const button of pressedButtons) {
+      console.log(button);
+      if (droneControlMode === false && button.id == 9) {
+        connectDrone();
+        droneControlMode = true;
+      } else {
+        droneController(button.id);
+      }
+    }
+  }
 
-	function isPressed({ button: { pressed } }) {
-		return !!pressed;
-	}
+  function isPressed({ button: { pressed } }) {
+    return !!pressed;
+  }
 
-	function log(message) {
-		const date = new Date().toISOString();
-		output.innerHTML += `${date}: ${message}\n`;
-	}
+  function log(message) {
+    const date = new Date().toISOString();
+    output.innerHTML += `${date}: ${message}\n`;
+  }
 
-	function droneController(buttonID) {
-		let on = null;
-		
-		switch (buttonID) {
-			case 0:
-				takeOff();
-				//log("Takeoff!");
-				break;
-			case 1:
-				landDrone();
-				//log("Landing.");
-				break;
-			case 4:
-				if (videoMode === false) {
-					videoOn();
-				}
-				break;
+  function droneController(buttonID) {
+    let on = null;
 
-			case 5:
-				if (videoMode === true) {
-					videoOff();
-				}
-				break;
-
-			default:
-				//log(`Button ${buttonID} was pressed.`);
-				break;
-		}
-	}
-	/*
+    switch (buttonID) {
+      case 0:
+        takeOff();
+        break;
+      case 1:
+        landDrone();
+        break;
+      case 4:
+        if (videoMode === false) {
+          videoOn();
+        }
+        break;
+      case 5:
+        if (videoMode === true) {
+          videoOff();
+        }
+        break;
+      default:
+        //log(`Button ${buttonID} was pressed.`);
+        break;
+    }
+  }
+  /*
 
 
 	Here's the drone command stuff. Hook it up later.
@@ -107,164 +102,164 @@ $(document).ready(function()
 
 
 	*/
-	function connectDrone() {
-		let connectionStatus = activateDrone();
+  function connectDrone() {
+    let connectionStatus = activateDrone();
 
-		if (connectionStatus == 1) {
-			let connectButton = document.getElementById("connectDrone");
-			let commands = document.getElementById("droneCommands");
-			commands.style.display = "inline";
-			connectButton.style.display = "none";
-		} else if (connectionStatus == 0) {
-			alert("Connection failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+    if (connectionStatus == 1) {
+      let connectButton = document.getElementById("connectDrone");
+      let commands = document.getElementById("droneCommands");
+      commands.style.display = "inline";
+      connectButton.style.display = "none";
+    } else if (connectionStatus == 0) {
+      alert("Connection failed.");
+    } else {
+      alert("Drone not found.");
+    }
+  }
 });
 
-	function videoOn() {
-		let success = activateVideo();
+function videoOn() {
+  let success = activateVideo();
 
-		if (success == 1) {
-			let on = document.getElementById("videoOn");
-			let off = document.getElementById("videoOff");
+  if (success == 1) {
+    let on = document.getElementById("videoOn");
+    let off = document.getElementById("videoOff");
 
-			on.style.display = "none";
-			off.style.display = "inline";
-		} else if (connectionStatus == 0) {
-			alert("Video failed.");
-		} else {
-			alert("Drone not found.");
-		}
+    on.style.display = "none";
+    off.style.display = "inline";
+  } else if (connectionStatus == 0) {
+    alert("Video failed.");
+  } else {
+    alert("Drone not found.");
+  }
 
-		videoMode = true;
-	}
+  videoMode = true;
+}
 
-	function videoOff() {
-		let success = deactivateVideo();
+function videoOff() {
+  let success = deactivateVideo();
 
-		if (success == 1) {
-			let on = document.getElementById("videoOn");
-			let off = document.getElementById("videoOff");
+  if (success == 1) {
+    let on = document.getElementById("videoOn");
+    let off = document.getElementById("videoOff");
 
-			on.style.display = "inline";
-			off.style.display = "none";
-		} else if (connectionStatus == 0) {
-			alert("Video failed.");
-		} else {
-			alert("Drone not found.");
-		}
-		
-		videoMode = false;
-	}
+    on.style.display = "inline";
+    off.style.display = "none";
+  } else if (connectionStatus == 0) {
+    alert("Video failed.");
+  } else {
+    alert("Drone not found.");
+  }
 
-	function takeOff() {
-		let success = takeoff();
+  videoMode = false;
+}
 
-		if (success == 1) {
-		} else if (success == 0) {
-			alert("Take off failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+function takeOff() {
+  let success = takeoff();
 
-	function landDrone() {
-		let success = land();
+  if (success == 1) {
+  } else if (success == 0) {
+    alert("Take off failed.");
+  } else {
+    alert("Drone not found.");
+  }
+}
 
-		if (success == 1) {
-		} else if (success == 0) {
-			alert("Landing failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+function landDrone() {
+  let success = land();
 
-	function up() {
-		let success = flyUp(20);
+  if (success == 1) {
+  } else if (success == 0) {
+    alert("Landing failed.");
+  } else {
+    alert("Drone not found.");
+  }
+}
 
-		if (success == 1) {
-		} else if (success == 0) {
-			alert("Up failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+function up() {
+  let success = flyUp(20);
 
-	function down() {
-		let success = flyDown(20);
+  if (success == 1) {
+  } else if (success == 0) {
+    alert("Up failed.");
+  } else {
+    alert("Drone not found.");
+  }
+}
 
-		if (success == 1) {
-		} else if (success == 0) {
-			alert("Down failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+function down() {
+  let success = flyDown(20);
 
-	function forward() {
-		let success = flyForward(20);
+  if (success == 1) {
+  } else if (success == 0) {
+    alert("Down failed.");
+  } else {
+    alert("Drone not found.");
+  }
+}
 
-		if (success == 1) {
-		} else if (success == 0) {
-			alert("Forward failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+function forward() {
+  let success = flyForward(20);
 
-	function back() {
-		let success = flyBackward(20);
+  if (success == 1) {
+  } else if (success == 0) {
+    alert("Forward failed.");
+  } else {
+    alert("Drone not found.");
+  }
+}
 
-		if (success == 1) {
-		} else if (success == 0) {
-			alert("Back failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+function back() {
+  let success = flyBackward(20);
 
-	function left() {
-		let success = flyLeft(20);
+  if (success == 1) {
+  } else if (success == 0) {
+    alert("Back failed.");
+  } else {
+    alert("Drone not found.");
+  }
+}
 
-		if (success == 1) {
-		} else if (success == 0) {
-			alert("Left failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+function left() {
+  let success = flyLeft(20);
 
-	function right() {
-		let success = flyRight(20);
+  if (success == 1) {
+  } else if (success == 0) {
+    alert("Left failed.");
+  } else {
+    alert("Drone not found.");
+  }
+}
 
-		if (success == 1) {
-		} else if (success == 0) {
-			alert("Right failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+function right() {
+  let success = flyRight(20);
 
-	function clockwise() {
-		let success = rotateCW(10);
+  if (success == 1) {
+  } else if (success == 0) {
+    alert("Right failed.");
+  } else {
+    alert("Drone not found.");
+  }
+}
 
-		if (success == 1) {
-		} else if (success == 0) {
-			alert("Clockwise rotation failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+function clockwise() {
+  let success = rotateCW(10);
 
-	function counterClockwise() {
-		let success = rotateCCW(10);
+  if (success == 1) {
+  } else if (success == 0) {
+    alert("Clockwise rotation failed.");
+  } else {
+    alert("Drone not found.");
+  }
+}
 
-		if (success == 1) {
-		} else if (success == 0) {
-			alert("Counter clockwise rotation failed.");
-		} else {
-			alert("Drone not found.");
-		}
-	}
+function counterClockwise() {
+  let success = rotateCCW(10);
+
+  if (success == 1) {
+  } else if (success == 0) {
+    alert("Counter clockwise rotation failed.");
+  } else {
+    alert("Drone not found.");
+  }
+}
